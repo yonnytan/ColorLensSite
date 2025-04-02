f981e5145918925c89ba74be42f33e0e0047f0b9
Yonatan Belihu <belihuyonatan@gmail.com>
2025-04-02 19:11:24 -0400

    updated favicon

--- src/components/common/Favicon.jsx
+++ src/components/common/Favicon.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
-import colorLensLogo from '../../assets/colorlenslogo.png';
+// Update the import path to where the image actually exists
+// For example, if it's in the public folder:
+// No import needed for files in public folder
 
 export function Favicon() {
   useEffect(() => {
@@ -7,7 +9,8 @@ export function Favicon() {
     const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/png';
     link.rel = 'shortcut icon';
-    link.href = colorLensLogo;
+    // Use the path to the image in the public folder
+    link.href = '/colorlenslogo.png'; // Assuming it's in the public root
     
     // Add it to the document head
     document.getElementsByTagName('head')[0].appendChild(link);
